c83d265ae4a6baaf7b2a1be2ada043e7f83035d7
gabrielecosta <67752197+gabrielecosta@users.noreply.github.com>
2020-07-12 10:52:12 +0200

    Added in app navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { ProductsComponent } from './products/products.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'aboutus', component: AboutUsComponent},
+  { path: 'contactus', component: ContactUsComponent},
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from "./products";
 
 @Component({
   selector: 'app-products',
@@ -9,7 +10,15 @@ export class ProductsComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
   }
+
+ products: Product[] = [
+   { id: 1, name: "Computer", price: 250 },
+   { id: 2, name: "Smartphone", price: 899.99 },
+   { id: 3, name: "Cover", price: 29.99}
+ ];
+
 
 }
